9c7dfca60576f922ff603f472c6fbe02b6f20a4e
CristianAlvarez <cristianandres1496@gmail.com>
2025-12-26 17:53:36 -0500

    checkout component

--- src/customer/pages/checkout/AddressForm.tsx
+++ src/customer/pages/checkout/AddressForm.tsx
@@ -1,6 +1,21 @@
 import { Box, Button, Grid, Modal, TextField, Typography } from "@mui/material";
 import { useFormik } from "formik";
 import React, { useState } from "react";
+import * as Yup from "yup";
+
+const AddressFormSchema = Yup.object().shape({
+  name: Yup.string().required("Name is required"),
+  mobile: Yup.string()
+    .required("Mobile number is required")
+    .matches(/^[6-9]\d{9}$/, "Invalid mobile number"),
+  pinCode: Yup.string()
+    .required("Pin code is required")
+    .matches(/^[1-9][0-9]{6}$/, "Invalid pin code"),
+  address: Yup.string().required("Address is required"),
+  city: Yup.string().required("City is required"),
+  state: Yup.string().required("State is required"),
+  locality: Yup.string().required("Locality is required"),
+});
 
 const AddressForm = () => {
   const formik = useFormik({
@@ -13,27 +28,29 @@ const AddressForm = () => {
       state: "",
       locality: "",
     },
-    validationSchema: {},
-    onSubmit: (values) => {},
+    validationSchema: AddressFormSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
   });
 
   return (
     <Box sx={{ max: "auto" }}>
       <p className="text-xl font-bold text-center pb-5">Contact Details</p>
-      <form>
-        <Grid container spacing={8}>
+      <form onSubmit={formik.handleSubmit}>
+        <Grid container spacing={3}>
           <Grid size={{ xs: 12 }}>
             <TextField
               fullWidth
               name="name"
-              label="name"
+              label="Name"
               value={formik.values.name}
               onChange={formik.handleChange}
               error={formik.touched.name && Boolean(formik.errors.name)}
-              helperText={formik.touched.name && Boolean(formik.errors.name)}
+              helperText={formik.errors.name}
             />
           </Grid>
-          <Grid size={{ xs: 12 }}>
+          <Grid size={{ xs: 6 }}>
             <TextField
               fullWidth
               name="mobile"
@@ -41,12 +58,10 @@ const AddressForm = () => {
               value={formik.values.mobile}
               onChange={formik.handleChange}
               error={formik.touched.mobile && Boolean(formik.errors.mobile)}
-              helperText={
-                formik.touched.mobile && Boolean(formik.errors.mobile)
-              }
+              helperText={formik.errors.mobile}
             />
           </Grid>
-          <Grid size={{ xs: 12 }}>
+          <Grid size={{ xs: 6 }}>
             <TextField
               fullWidth
               name="pinCode"
@@ -54,9 +69,7 @@ const AddressForm = () => {
               value={formik.values.pinCode}
               onChange={formik.handleChange}
               error={formik.touched.pinCode && Boolean(formik.errors.pinCode)}
-              helperText={
-                formik.touched.pinCode && Boolean(formik.errors.pinCode)
-              }
+              helperText={formik.errors.pinCode}
             />
           </Grid>
           <Grid size={{ xs: 12 }}>
@@ -67,9 +80,7 @@ const AddressForm = () => {
               value={formik.values.address}
               onChange={formik.handleChange}
               error={formik.touched.address && Boolean(formik.errors.address)}
-              helperText={
-                formik.touched.address && Boolean(formik.errors.address)
-              }
+              helperText={formik.errors.address}
             />
           </Grid>
           <Grid size={{ xs: 12 }}>
@@ -80,12 +91,10 @@ const AddressForm = () => {
               value={formik.values.locality}
               onChange={formik.handleChange}
               error={formik.touched.locality && Boolean(formik.errors.locality)}
-              helperText={
-                formik.touched.locality && Boolean(formik.errors.locality)
-              }
+              helperText={formik.errors.locality}
             />
           </Grid>
-          <Grid size={{ xs: 12 }}>
+          <Grid size={{ xs: 6 }}>
             <TextField
               fullWidth
               name="city"
@@ -93,10 +102,10 @@ const AddressForm = () => {
               value={formik.values.city}
               onChange={formik.handleChange}
               error={formik.touched.city && Boolean(formik.errors.city)}
-              helperText={formik.touched.city && Boolean(formik.errors.city)}
+              helperText={formik.errors.city}
             />
           </Grid>
-          <Grid size={{ xs: 12 }}>
+          <Grid size={{ xs: 6 }}>
             <TextField
               fullWidth
               name="state"
@@ -104,8 +113,18 @@ const AddressForm = () => {
               value={formik.values.state}
               onChange={formik.handleChange}
               error={formik.touched.state && Boolean(formik.errors.state)}
-              helperText={formik.touched.state && Boolean(formik.errors.state)}
+              helperText={formik.errors.state}
             />
+          </Grid>
+          <Grid size={{ xs: 12 }}>
+            <Button
+              fullWidth
+              type="submit"
+              variant="contained"
+              sx={{ py: "14px" }}
+            >
+              Add address
+            </Button>
           </Grid>
         </Grid>
       </form>
--- src/customer/pages/checkout/Checkout.tsx
+++ src/customer/pages/checkout/Checkout.tsx
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
 import AddressCard from "./AddressCard";
-import { Box, Button, Modal, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControlLabel,
+  Modal,
+  Radio,
+  RadioGroup,
+  Typography,
+} from "@mui/material";
 import AddressForm from "./AddressForm";
+import PricingCart from "../cart/PricingCart";
 
 const style = {
   position: "absolute",
@@ -15,10 +24,28 @@ const style = {
   p: 4,
 };
 
+const paymentGatwayList = [
+  {
+    value: "RAZORPAY",
+    image: "https://cdn.pixabay.com/photo/2015/04/15/09/28/head-723540_640.jpg",
+    label: "",
+  },
+  {
+    value: "STRIPE",
+    image: "https://cdn.pixabay.com/photo/2015/04/15/09/28/head-723540_640.jpg",
+    label: "",
+  },
+];
+
 const Checkout = () => {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [paymentGatway, setPaymentGatway] = useState("RAZORPAY");
+
+  const handlePaymentChange = (e: any) => {
+    setPaymentGatway(e.target.value);
+  };
 
   return (
     <>
@@ -39,6 +66,48 @@ const Checkout = () => {
             </div>
             <div className="py-4 px-5 rounded-md border border-gray-300">
               <Button onClick={handleOpen}>Add new Address</Button>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div className="space-y-3 border border-gray-400 p-5 rounded-md">
+                <h1 className="text-teal-600 font-medium my-2 text-center">
+                  Chose Payment Gatway
+                </h1>
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-row-radio-buttons-group-label"
+                  name="row-radio-buttons-group"
+                  className="flex items-center justify-between mx-4"
+                  onChange={handlePaymentChange}
+                  value={paymentGatway}
+                >
+                  {paymentGatwayList.map((e) => (
+                    <FormControlLabel
+                      className="border border-gray-400 w-[45%] px-2 rounded-md flex justify-center"
+                      value={e.value}
+                      control={<Radio />}
+                      label={
+                        <img
+                          className={`${
+                            e.value === "stripe" ? "w-14 " : "w-14 "
+                          } object-cover`}
+                          src={e.image}
+                          alt={e.label}
+                        />
+                      }
+                    />
+                  ))}
+                </RadioGroup>
+              </div>
+            </div>
+            <div className="border border-gray-400 rounded-md">
+              <PricingCart />
+              <div className="p-5">
+                <Button fullWidth variant="contained" sx={{ py: "11px" }}>
+                  Checkout
+                </Button>
+              </div>
             </div>
           </div>
         </div>
