6bcb4f1adf4576c5d6ffb8b8fdc7ef05c529bbe6
cristian <cristianalvarez1496@gmail.com>
2026-01-07 17:24:55 -0500

    product form

--- src/seller/pages/Products/AddProduct.tsx
+++ src/seller/pages/Products/AddProduct.tsx
@@ -1,6 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { menLevelThree } from '../../../data/category/levelThree/menLevelThree'
+import { womenLevelThree } from '../../../data/category/levelThree/womenLevelThree'
+import { fornitureLevelThree } from '../../../data/category/levelThree/fornitureLevelThree'
+import { electronicLevelThree } from '../../../data/category/levelThree/electronicLevelThree'
+import { menLevelTwo } from '../../../data/category/levelTwo/menLevelTwo'
+import { womenLevelTwo } from '../../../data/category/levelTwo/womenLevelTwo'
+import { fornitureLevelTwo } from '../../../data/category/levelTwo/fornitureLevelTwo'
+import { electronicLevelTwo } from '../../../data/category/levelTwo/electronicLevelTwo'
+import { useFormik } from 'formik'
+import { uploadToCloudinary } from '../../../util/uploadToCloudinary'
+
+const categoryTwo:{[key: string]:any[]} = {
+  men:menLevelTwo,
+  women:womenLevelTwo,
+  kids:[],
+  home_furniture:fornitureLevelTwo,
+  beauty: [],
+  electronics: electronicLevelTwo
+}
+const categoryThree:{[key: string]:any[]} = {
+  men:menLevelThree,
+  women:womenLevelThree,
+  kids:[],
+  home_furniture:fornitureLevelThree,
+  beauty: [],
+  electronics: electronicLevelThree
+}
 
 const AddProduct = () => {
+  const [uploadImage, setUploadingImage] = useState(false);
+  const [snackbarOpen, setOpenSnackbar] = useState(false);
+
+  const formik = useFormik({
+    initialValues:{
+      title:"",
+      descripcion:"",
+      mrpPrice:"",
+      sellingPrice:"",
+      quantity:"",
+      color:"",
+      images:[],
+      category:"",
+      category2:"",
+      category3:"",
+      sizes:""
+    },
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
+
+  const handleImageChange = async (event:any) => {
+    const file = event.target.files[0];
+    setUploadingImage(true)
+    const image = await uploadToCloudinary(file)
+    
+    formik.setFieldValue("images",[...formik.values.images, image]);
+    setUploadingImage(false)
+  }
+
+  const handleRemoveImage = async(index:any) => {
+    
+  }
+
+  const childCategory = (category:any, parentCategory:any) => {
+    // return category.filter((child:any) => {
+    //   return child.parentCategoryId == parentCategoryId;
+    // })
+  }
+
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false);
+  }
+
   return (
     <div>AddProduct</div>
   )
---
+++ src/util/uploadToCloudinary.ts
@@ -0,0 +1,23 @@
+
+export const uploadToCloudinary = async (pics:any) => {
+  const cloud_name = import.meta.env.VITE_CLOUD_NAME;
+  const upload_preset = import.meta.env.VITE_UPLOAD_PRESET;
+  const cloudinary_API_URL = `"https://api.cloudinary.com/v1_1/${cloud_name}/image/${upload_preset}"`
+  
+  if(pics){
+    const data = new FormData();
+    data.append("file",pics)
+    data.append("upload_preset",upload_preset);
+    data.append("cloud_name",cloud_name);
+
+    const res = await fetch(cloudinary_API_URL, {
+      method:"POST",
+      body:data
+    })
+
+    const fileData = await res.json();
+    return fileData.url;
+  }else {
+    console.log("error : pics not found");
+  }
+}
