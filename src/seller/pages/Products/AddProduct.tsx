28552b2be1d39b38fd7ee82cc9bd4525ebae99b3
cristian <cristianalvarez1496@gmail.com>
2026-01-09 18:15:31 -0500

    form addproduct

--- src/seller/pages/Products/AddProduct.tsx
+++ src/seller/pages/Products/AddProduct.tsx
@@ -1,81 +1,368 @@
-import React, { useState } from 'react'
-import { menLevelThree } from '../../../data/category/levelThree/menLevelThree'
-import { womenLevelThree } from '../../../data/category/levelThree/womenLevelThree'
-import { fornitureLevelThree } from '../../../data/category/levelThree/fornitureLevelThree'
-import { electronicLevelThree } from '../../../data/category/levelThree/electronicLevelThree'
-import { menLevelTwo } from '../../../data/category/levelTwo/menLevelTwo'
-import { womenLevelTwo } from '../../../data/category/levelTwo/womenLevelTwo'
-import { fornitureLevelTwo } from '../../../data/category/levelTwo/fornitureLevelTwo'
-import { electronicLevelTwo } from '../../../data/category/levelTwo/electronicLevelTwo'
-import { useFormik } from 'formik'
-import { uploadToCloudinary } from '../../../util/uploadToCloudinary'
+import React, { useState } from "react";
+import { menLevelThree } from "../../../data/category/levelThree/menLevelThree";
+import { womenLevelThree } from "../../../data/category/levelThree/womenLevelThree";
+import { fornitureLevelThree } from "../../../data/category/levelThree/fornitureLevelThree";
+import { electronicLevelThree } from "../../../data/category/levelThree/electronicLevelThree";
+import { menLevelTwo } from "../../../data/category/levelTwo/menLevelTwo";
+import { womenLevelTwo } from "../../../data/category/levelTwo/womenLevelTwo";
+import { fornitureLevelTwo } from "../../../data/category/levelTwo/fornitureLevelTwo";
+import { electronicLevelTwo } from "../../../data/category/levelTwo/electronicLevelTwo";
+import { useFormik } from "formik";
+import { uploadToCloudinary } from "../../../util/uploadToCloudinary";
+import { Button, CircularProgress, FormControl, FormHelperText, Grid, IconButton, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+import { AddPhotoAlternate, Close } from "@mui/icons-material";
+import { mainCategory } from "../../../data/category/mainCategory";
+import { colors } from "../../../data/filter/color";
 
-const categoryTwo:{[key: string]:any[]} = {
-  men:menLevelTwo,
-  women:womenLevelTwo,
-  kids:[],
-  home_furniture:fornitureLevelTwo,
+const categoryTwo: { [key: string]: any[] } = {
+  men: menLevelTwo,
+  women: womenLevelTwo,
+  kids: [],
+  home_furniture: fornitureLevelTwo,
   beauty: [],
-  electronics: electronicLevelTwo
-}
-const categoryThree:{[key: string]:any[]} = {
-  men:menLevelThree,
-  women:womenLevelThree,
-  kids:[],
-  home_furniture:fornitureLevelThree,
+  electronics: electronicLevelTwo,
+};
+const categoryThree: { [key: string]: any[] } = {
+  men: menLevelThree,
+  women: womenLevelThree,
+  kids: [],
+  home_furniture: fornitureLevelThree,
   beauty: [],
-  electronics: electronicLevelThree
-}
+  electronics: electronicLevelThree,
+};
 
 const AddProduct = () => {
   const [uploadImage, setUploadingImage] = useState(false);
   const [snackbarOpen, setOpenSnackbar] = useState(false);
 
   const formik = useFormik({
-    initialValues:{
-      title:"",
-      descripcion:"",
-      mrpPrice:"",
-      sellingPrice:"",
-      quantity:"",
-      color:"",
-      images:[],
-      category:"",
-      category2:"",
-      category3:"",
-      sizes:""
+    initialValues: {
+      title: "",
+      descripcion: "",
+      mrpPrice: "",
+      sellingPrice: "",
+      quantity: "",
+      color: "",
+      images: [],
+      category: "",
+      category2: "",
+      category3: "",
+      sizes: "",
     },
     onSubmit: (values) => {
-      console.log(values)
-    }
-  })
+      console.log(values);
+    },
+  });
 
-  const handleImageChange = async (event:any) => {
+  const handleImageChange = async (event: any) => {
     const file = event.target.files[0];
-    setUploadingImage(true)
-    const image = await uploadToCloudinary(file)
-    
-    formik.setFieldValue("images",[...formik.values.images, image]);
-    setUploadingImage(false)
-  }
+    setUploadingImage(true);
+    const image = await uploadToCloudinary(file);
 
-  const handleRemoveImage = async(index:any) => {
-    
-  }
+    formik.setFieldValue("images", [...formik.values.images, image]);
+    setUploadingImage(false);
+  };
 
-  const childCategory = (category:any, parentCategory:any) => {
+  const handleRemoveImage = async (index: any) => {
+    const updatedImages = [...formik.values.images];
+    updatedImages.splice(index, 1);
+    formik.setFieldValue("images", updatedImages);
+  };
+
+  const childCategory = (category: any, parentCategory: any) => {
     // return category.filter((child:any) => {
     //   return child.parentCategoryId == parentCategoryId;
     // })
-  }
+  };
 
   const handleCloseSnackbar = () => {
     setOpenSnackbar(false);
-  }
+  };
 
   return (
-    <div>AddProduct</div>
-  )
-}
+    <div>
+      <form onSubmit={formik.handleSubmit} className="space-y-4 p-4">
+        <Grid container spacing={2}>
+          <Grid className="flex flex-wrap gap-5" size={{xs:12}}>
+            <input
+              type="file"
+              accept="image/*"
+              id="fileInput"
+              style={{ display: "none" }}
+              onChange={handleImageChange}
+            />
+            <label className="relative" htmlFor="fileInput">
+              <span className="w-24 h-24 cursor-pointer flex items-center justify-center p-3 border rounded-md border-gray-400">
+                <AddPhotoAlternate className="text-gray-700" />
+              </span>
+              {uploadImage && (
+                <div className="absolute left-0 right-0 top-0 bottom-0 w-24 h-24 flex justify-center items-center">
+                  <CircularProgress />
+                </div>
+              )}
+            </label>
+            <div className="flex flex-wrap gap-2">
+              {formik.values.images.map((image, index) => (
+                <div className="relative">
+                  <img className="w-24 h-24 object-cover" key={index} src={image} alt={`ProductImage ${index + 1}`}/>
+                  <IconButton
+                    onClick={() => handleRemoveImage(index)}
+                    className=""
+                    size="small"
+                    color="error"
+                    sx={{
+                      position:"absolute",
+                      top:0,
+                      right:0,
+                      outline:"none"
+                    }}
+                  >
+                    <Close sx={{ fontSize:"1rem"}}/>
+                  </IconButton>
+                </div>
+              ))
+              }
+            </div>
+          </Grid>
+          <Grid size={{xs:12}}>
+            <TextField
+              multiline
+              rows={4}
+              fullWidth
+              id="title"
+              name="title"
+              label="Title"
+              value={formik.values.title}
+              onChange={formik.handleChange}
+              error= {
+                formik.touched.title && Boolean(formik.errors.title)
+              }
+              helperText={
+                formik.touched.title && formik.errors.title
+              }
+              required
+            />
 
-export default AddProduct
+          </Grid>
+          <Grid size={{xs:12}}>
+            <TextField
+              multiline
+              rows={4}
+              fullWidth
+              id="description"
+              name="description"
+              label="Description"
+              value={formik.values.descripcion}
+              onChange={formik.handleChange}
+              error= {
+                formik.touched.descripcion && Boolean(formik.errors.descripcion)
+              }
+              helperText={
+                formik.touched.descripcion && formik.errors.descripcion
+              }
+              required
+            />
+
+          </Grid>
+          <Grid size={{xs:12}}>
+            <TextField
+              fullWidth
+              id="mpr_Price"
+              name="mprPrice"
+              label="MPR Price"
+              type="number"
+              value={formik.values.mrpPrice}
+              onChange={formik.handleChange}
+              error= {
+                formik.touched.mrpPrice && Boolean(formik.errors.mrpPrice)
+              }
+              helperText={
+                formik.touched.mrpPrice && formik.errors.mrpPrice
+              }
+              required
+            />
+
+          </Grid>
+          <Grid size={{xs:12}}>
+            <TextField
+              fullWidth
+              id="sellingPrice"
+              name="sellingPrice"
+              label="Selling Price"
+              type="number"
+              value={formik.values.sellingPrice}
+              onChange={formik.handleChange}
+              error= {
+                formik.touched.sellingPrice && Boolean(formik.errors.sellingPrice)
+              }
+              helperText={
+                formik.touched.sellingPrice && formik.errors.sellingPrice
+              }
+              required
+            />
+
+          </Grid>
+          <Grid size={{xs:12}}>
+              <FormControl fullWidth error={formik.touched.sizes && Boolean(formik.errors.sizes)} required>
+                <InputLabel id="color-label">Color</InputLabel>
+                <Select
+                  labelId="color-label"
+                  id="color"
+                  name="color"
+                  value={formik.values.color}
+                  onChange={formik.handleChange}
+                  label="Color"
+                > 
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  {
+                    colors.map((color,index) => <MenuItem value={color.name}>
+                      <div className="flex gap-3">
+                        <span style={{backgroundColor:color.hex}} className={`h-5 w-5 rounded-full ${color.name === "White" ? "border" : ""}`} ></span>
+                        <p>{color.name}</p>
+                      </div>
+                    </MenuItem>)
+                  }
+                </Select>
+                {
+                  formik.touched.sizes && formik.errors.sizes && (
+                    <FormHelperText>{formik.errors.sizes}</FormHelperText>
+                  )
+                }
+              </FormControl>
+          </Grid>
+          <Grid size={{xs:12}}>
+              <FormControl fullWidth error={formik.touched.sizes && Boolean(formik.errors.sizes)} required>
+                <InputLabel id="sizes-label">Sizes</InputLabel>
+                <Select
+                  labelId="sizes-label"
+                  id="sizes"
+                  name="sizes"
+                  value={formik.values.sizes}
+                  onChange={formik.handleChange}
+                  label="Sizes"
+                > 
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value="FREE">FREE</MenuItem>
+                  <MenuItem value="S">S</MenuItem>
+                  <MenuItem value="M">M</MenuItem>
+                  <MenuItem value="L">L</MenuItem>
+                  <MenuItem value="XL">XL</MenuItem>
+                </Select>
+                {
+                  formik.touched.sizes && formik.errors.sizes && (
+                    <FormHelperText>{formik.errors.sizes}</FormHelperText>
+                  )
+                }
+              </FormControl>
+          </Grid>
+          <Grid size={{xs:12}}>
+            <FormControl
+              fullWidth
+              error={formik.touched.category && Boolean(formik.errors.category)}
+              required
+            > 
+              <InputLabel id="category-label">Category</InputLabel>
+              <Select
+                labelId="category-label"
+                id="category"
+                name="category"
+                value={formik.values.category2}
+                onChange={formik.handleChange}
+                label="Category"
+              >
+                {/* <MenuItem value="">
+                  <em>None</em>
+                </MenuItem> */}
+                {mainCategory?.map((item:any) => (
+                    <MenuItem value={item.categoryId}>{item.name}</MenuItem>
+                  ))
+                }
+              </Select>
+              {formik.touched.category && formik.errors.category && (
+                <FormHelperText>{formik.errors.category}</FormHelperText>
+              )
+              }
+            </FormControl>
+          </Grid>
+          <Grid size={{xs:12}}>
+            <FormControl
+              fullWidth
+              error={formik.touched.category && Boolean(formik.errors.category)}
+              required
+            > 
+              <InputLabel id="category2-label">Second Category</InputLabel>
+              <Select
+                labelId="category2-label"
+                id="category2"
+                name="category2"
+                value={formik.values.category2}
+                onChange={formik.handleChange}
+                label="Second Category"
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                {formik.values.category2 &&
+                  categoryTwo[formik.values.category]?.map((item:any) => (
+                    <MenuItem value={item.categoryId}>{item.name}</MenuItem>
+                  ))
+                }
+              </Select>
+              {formik.touched.category && formik.errors.category && (
+                <FormHelperText>{formik.errors.category}</FormHelperText>
+              )
+              }
+            </FormControl>
+          </Grid>
+          <Grid size={{xs:12}}>
+            <FormControl
+              fullWidth
+              error={formik.touched.category && Boolean(formik.errors.category)}
+              required
+            > 
+              <InputLabel id="category-label">Third Category</InputLabel>
+              <Select
+                labelId="category-label"
+                id="category"
+                name="category3"
+                value={formik.values.category3}
+                onChange={formik.handleChange}
+                label="Third Category"
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                {formik.values.category2 &&
+                  childCategory(categoryThree[formik.values.category],formik.values.category2)?.map((item:any) => (
+                    <MenuItem value={item.categoryId}>{item.name}</MenuItem>
+                  ))
+                }
+              </Select>
+              {formik.touched.category && formik.errors.category && (
+                <FormHelperText>{formik.errors.category}</FormHelperText>
+              )
+              }
+            </FormControl>
+          </Grid>
+          <Grid  size={{xs:12}}>
+              <Button
+                sx={{p:"14px"}}
+                color="primary"
+                variant="contained"
+                fullWidth
+                type="submit"
+              >
+                {false ? <CircularProgress size="small" sx={{ width:"27px", height:"27px"}}/> : "Add Product"}
+              </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+  );
+};
+
+export default AddProduct;
