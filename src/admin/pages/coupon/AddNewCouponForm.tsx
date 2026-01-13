85d3972a41079eac87d5bf93f1d858fc1c78ecd2
cristian <cristianalvarez1496@gmail.com>
2026-01-13 16:41:13 -0500

    coupon component

--- src/admin/pages/coupon/AddNewCouponForm.tsx
+++ src/admin/pages/coupon/AddNewCouponForm.tsx
@@ -1,11 +1,106 @@
-import React from 'react'
+import { Box, Button, Grid, TextField } from "@mui/material";
+import { teal } from "@mui/material/colors";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import type { Dayjs } from "dayjs";
+import { useFormik } from "formik";
+import React from "react";
 
-const AddNewCouponForm = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+interface CouponFormValues {
+  code: string;
+  discountPercentage: number;
+  validityStartDate: Dayjs | null;
+  validityEndDate: Dayjs | null;
+  minimumOrderValue: number;
 }
 
-export default AddNewCouponForm
+const AddNewCouponForm = () => {
+  const formik = useFormik<CouponFormValues>({
+    initialValues: {
+      code: "",
+      discountPercentage: 0,
+      validityStartDate: null,
+      validityEndDate: null,
+      minimumOrderValue: 0,
+    },
+    onSubmit: (values) => {
+      const formatedValues = {
+        ...values,
+        validityStartDate: values.validityStartDate?.toISOString(),
+        validityEndDate: values.validityEndDate?.toISOString(),
+      };
+      console.log("form submited ", values, formatedValues);
+    },
+  });
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-teal-600 pb-5 text-center">Create New Coupon</h1>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Box component={"form"} onSubmit={formik.handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid size={{ xs: 12, sm: 16 }}>
+              <TextField
+                fullWidth
+                name="code"
+                label="coupon code"
+                value={formik.values.code}
+                onChange={formik.handleChange}
+                error={formik.touched.code && Boolean(formik.errors.code)}
+                helperText={formik.errors.code}
+              />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 16 }}>
+              <TextField
+                fullWidth
+                name="discountPercentage"
+                label="Discount Percentage"
+                value={formik.values.discountPercentage}
+                onChange={formik.handleChange}
+                error={formik.touched.discountPercentage && Boolean(formik.errors.discountPercentage)}
+                helperText={formik.errors.discountPercentage}
+              />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <DatePicker
+                sx={{width:"100%"}}
+                label="Validity Start Date"
+                name="validityStartDate"
+                onChange={formik.handleChange}
+                value={formik.values.validityStartDate}
+              />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <DatePicker
+                sx={{width:"100%"}}
+                label="Validity End Date"
+                name="validityEndDate"
+                onChange={formik.handleChange}
+                value={formik.values.validityStartDate}
+              />
+            </Grid>
+
+            <Grid size={{ xs: 12 }}>
+              <TextField
+                fullWidth
+                name="minimumOrderValue"
+                label="Minimum Order Value"
+                value={formik.values.minimumOrderValue}
+                onChange={formik.handleChange}
+                error={formik.touched.minimumOrderValue && Boolean(formik.errors.minimumOrderValue)}
+                helperText={formik.errors.minimumOrderValue}
+              />
+            </Grid>
+            <Grid size={{xs:12}} >
+              <Button variant="contained" fullWidth sx={{py:".8rem"}}>
+                Create Coupon
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </LocalizationProvider>
+    </div>
+  );
+};
+
+export default AddNewCouponForm;
--- src/admin/pages/coupon/Coupon.tsx
+++ src/admin/pages/coupon/Coupon.tsx
@@ -1,11 +1,127 @@
-import React from 'react'
+import { useState } from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import { Button, Table, TableBody, TableContainer, TableHead, TableRow } from "@mui/material";
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import { Delete } from "@mui/icons-material";
 
-const Coupon = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+const accountStat = [
+  {
+    status: "PENDING_VERIFICATION",
+    title: "Pending Verification",
+    description: "",
+  },
+  {
+    status: "ACTIVE",
+    title: "Active",
+    description: "Account is",
+  },
+  {
+    status: "SUSPENDED",
+    title: "Suspended",
+    description: "Account is",
+  },
+  {
+    status: "DEACTIVATED",
+    title: "Deactivated",
+    description: "Account is",
+  },
+  {
+    status: "BANNED",
+    title: "Banned",
+    description: "Account is",
+  },
+  {
+    status: "CLOSED",
+    title: "Closed",
+    description: "Account is",
+  },
+];
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
 }
 
-export default Coupon
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export default function Coupon() {
+  const [accountStatus, setAccountStatus] = useState("ACTIVE");
+
+  const handleChange = (e: any) => {
+    setAccountStatus(e.target.value);
+  };
+
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Coupon Code</StyledTableCell>
+              <StyledTableCell>Start Date</StyledTableCell>
+              <StyledTableCell>End Date</StyledTableCell>
+              <StyledTableCell align="right">Minimum Order Value</StyledTableCell>
+              <StyledTableCell align="right">Discount</StyledTableCell>
+              <StyledTableCell align="right">Delete</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <StyledTableRow key={row.name}>
+                <StyledTableCell component="th" scope="row">
+                  {row.name}
+                </StyledTableCell>
+                <StyledTableCell>{row.calories}</StyledTableCell>
+                <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                <StyledTableCell align="right">{row.protein}</StyledTableCell>
+                <StyledTableCell align="right">
+                  <Button>
+                    <Delete/>
+                  </Button>
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
