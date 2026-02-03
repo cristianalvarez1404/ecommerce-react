64701487f57bb1680302f25add01a7752fbc3f0c
cristian <cristianalvarez1496@gmail.com>
2026-02-03 16:03:11 -0500

    order slice

--- src/state/customer/orderSlice.ts
+++ src/state/customer/orderSlice.ts
@@ -1,5 +1,105 @@
-import type { OrderState } from "../../types/orderTypes";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import type { Order, OrderItem, OrderState } from "../../types/orderTypes";
+import { api } from "../../config/Api";
+import type { Address } from "../../types/userTypes";
 
+export const fetchUserOrderHistory = createAsyncThunk<Order[], string>(
+  "/api/orders/fetchUserOrderHistory",
+  async (jwt, {rejectWithValue}) => {
+    try{
+      const response = await api.get<Order[]>("/api/orders/user", {
+        headers: {Authorization: `Bearer ${jwt}`},
+      })
+      return response.data;
+    }catch(error){
+      return rejectWithValue(
+        error || "Failed to fetch order history"
+      )
+    }
+  }
+)
+
+export const fetchOrderById = createAsyncThunk<Order, { orderId: number; jwt: string}>(
+  "orders/fetchOrderById",
+  async ({orderId, jwt},{rejectWithValue}) => {
+    try {
+      const response = await api.get<Order>(`/api/orders/${orderId}`, {
+        headers: { Authorization: `Bearer ${jwt}`}
+      })
+      return response.data;
+    }catch(error:any){
+      return rejectWithValue("Failed to fetch order")
+    }
+  }
+)
+
+export const createOrder = createAsyncThunk<any, {address: Address; jwt: string; paymentGateway: string}>(
+  "orders/createOrder",
+  async ({address, jwt, paymentGateway}, {rejectWithValue}) => {
+    try {
+      const response = await api.post("/api/orders", address, {
+        headers: {Authorization: `Bearer ${jwt}`},
+        params:{paymentGateway:paymentGateway}
+      })
+
+      if(response.data.payment_link_url){
+        window.location.href = response.data.payment_link_url;
+      }
+
+      return response.data;
+    }catch(error:any){
+      return rejectWithValue("Failed to create order");
+    }
+  }
+)
+
+export const fetchOrderItemById = createAsyncThunk<OrderItem, {orderItemId: number; jwt:string}>("orders/fetchOrderItemById", 
+  async ({orderItemId, jwt}, {rejectWithValue}) => {
+    try{
+      const response = await api.get<OrderItem>(`/api/orders/item/${orderItemId}`,{
+        headers: {Authorization: `Bearer ${jwt}`}
+      })
+
+      return response.data;
+    }catch(error:any){
+      return rejectWithValue("Failed to create order");
+    }
+  }
+)
+
+export const paymentSuccess = createAsyncThunk<any,{paymentId: string; jwt:string; paymentLinkId:string},{rejectValue:string}>(
+  "orders/paymentSuccess",
+  async ({paymentId, jwt, paymentLinkId},{rejectWithValue}) => {
+    try{
+      const response = await api.get(`/api/payment/${paymentId}`, {
+        headers: {
+          Authorization: `Bearer ${jwt}`
+        },
+        params:{paymentLinkId}
+      })
+      return response.data;
+    }catch(error:any){
+      if(error.response){
+        return rejectWithValue(error.response.data.message);
+      }
+      return rejectWithValue("Failed to proccess payment");
+    }
+  }
+)
+
+export const cancelOrder = createAsyncThunk<Order, any>(
+  "orders/cancelOrder",
+  async(orderId, {rejectWithValue}) => {
+    try{
+      const response = await api.put(`/api/orders/cancel`,{},{
+        headers:{Authorization:`Bearer ${localStorage.getItem("jwt")}`}
+      })
+      return response.data;
+    }catch(error:any){
+      return rejectWithValue("An error occurred while cancelling the order.")
+    }
+  }
+)
 
 const initialState: OrderState = {
   orders: [],
@@ -10,3 +110,39 @@ const initialState: OrderState = {
   error: null,
   orderCanceled: false,
 }
+
+const orderSlice = createSlice({
+  name: "orders",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUserOrderHistory.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+        state.orderCanceled = false;
+      })
+      .addCase(fetchUserOrderHistory.fulfilled, (state, action) => {
+        state.orders = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchUserOrderHistory.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
+      })
+
+      //Fetch order by ID
+      .addCase(fetchOrderById.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchOrderById.fulfilled, (state, action) => {
+        state.currentOrder = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchOrderById.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
+      })
+  }
+})
